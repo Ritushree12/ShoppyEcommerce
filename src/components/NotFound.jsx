28fb4668e42dad2fd28c691f82dd6172bf896c54
Ritushree L <56196065+Ritushree12@users.noreply.github.com>
2025-12-26 01:51:46 +0530

    Added an image for invlid routes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,6 +3,11 @@ import "../styles/styles.css";
 export default function NotFound() {
   return (
     <div className="not-found">
+      <img
+        src="/src/assets/404-error.png"
+        alt="404 Error"
+        className="error-image"
+      />
       <h2>404 — Page Not Found</h2>
       <p>The page you tried to access does not exist.</p>
     </div>
